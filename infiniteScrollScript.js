ca476068b3353ad7df740a990412b2cbe4a3ba09
LizHitches <liz.hitches@gmail.com>
2021-02-16 17:34:30 -0500

    added scroll

--- infiniteScrollScript.js
+++ infiniteScrollScript.js
@@ -12,7 +12,16 @@ const count= 10;
 const apiKey= 'NL4Y3T9Dq7L2SZwlivG7kBHpfgXOMlImPxIJOtCRoEE';
 const apiUrl= `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+// Helper Function to Set Attributes on DOM Elements 
 
+function setAttributes(element, attributes) {
+
+    for (const key in attributes) 
+    {
+        element.setAttribute(key, attributes[key])
+    }
+
+}
 //Create Elements for Links & Photos to DOM  
 
 function displayPhotos() {
@@ -22,18 +31,28 @@ photosArray.forEach((photo) => {
  
     // Create <a>  to link to Unsplash  
     const item= document.createElement('a');
-    item.setAttribute('href', photo.links.html);
-    item.setAttribute('target', '_blank'); 
+    // item.setAttribute('href', photo.links.html);
+    // item.setAttribute('target', '_blank'); 
 
-  
+  setAttributes(item, {
+      href: photo.links.html,  
+      target: '_blank ', 
+
+  });
 
 
     //Create  <img> for Photo 
 
     const img =document.createElement('img');
-    img.setAttribute('src', photo.urls.regular);
-    img.setAttribute('alt', photo.alt_description);
-    img.setAttribute('title', photo.alt_description);
+    // img.setAttribute('src', photo.urls.regular);
+    // img.setAttribute('alt', photo.alt_description);
+    // img.setAttribute('title', photo.alt_description);
+
+  setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description, 
+  })
 
     item.appendChild(img);
     imageContainer.appendChild(item);
@@ -54,4 +73,19 @@ async function getPhotos() {
         // catch error here
     }
 }
+// check to see if scrolling near bottom of page, load more photos 
+
+window.addEventListener('scroll', () => {
+    if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000)
+    {
+        getPhotos();
+        // console.log('window.innerHeight:', window.innerHeight);
+        // console.log('window.scrollY:', window.scrollY);
+        // console.log('window.innerHeight + scrollY:', window.scrollY +window.innerHeight);
+        // console.log('document.body.offsetHeight -1000:', document.body.offsetHeight - 1000);
+        console.log('load more');
+    }
+});
+ 
+//on load  
 getPhotos();
